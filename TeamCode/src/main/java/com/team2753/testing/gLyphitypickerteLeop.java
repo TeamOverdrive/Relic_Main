34641d921d88a18eb7d609084366dfeb5d0a08a1
David2753 <robodino2014@gmail.com>
2018-06-05 11:21:21 -0400

    Glyph Picker Finished
    Glyph Picker finished
    - changed map to match telemetry font
    (no longer changes drasticaly when glyphs are added or when pointer moves around)
    - Fixed pointer jumping to end of row or column
    - Basic position selecting is functional and done
    
    Need to work on outputing glyph position values.

--- TeamCode/src/main/java/com/team2753/testing/gLyphitypickerteLeop.java
+++ TeamCode/src/main/java/com/team2753/testing/gLyphitypickerteLeop.java
@@ -13,19 +13,24 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name = "Glyph Picker")
 
-public class gLyphitypickerteLeop extends Team2753Linear{
+public class gLyphitypickerteLeop extends Team2753Linear {
 
-    String[] glyphMap = {"+--------+--------+--------+",
-            "|\\       |        |       /|",
-            "| \\      |        |      / |",
-            "|  \\     |        |     /  |",
-            "|   \\    |        |    /   |",
-            "|    \\   |        |   /    |",
-            "|     \\  |        |  /     |",
-            "|      \\ |        | /      |",
-            "|       \\|        |/       |",
-            "+--------+--------+--------+"
-    };
+    /*
+    #________#________#________#
+    11_______1________1_______11
+    1_1______1________1______1_1
+    1__1_____1________1_____1__1
+    1___1____1________1____1___1
+    1____1___1________1___1____1
+    1_____1__1________1__1_____1
+    1______1_1________1_1______1
+    1_______11________11_______1
+    #________#________#________#
+     */
+
+    private static final char POINTER = '^';
+    private static final char GLYPH = 'g';
+
 
     //pointerMap probably unneeded
 
@@ -43,53 +48,32 @@ public class gLyphitypickerteLeop extends Team2753Linear{
     };
     */
 
-    String[] map = {"+--------+--------+--------+",
-            "|\\       |        |       /|",
-            "| \\      |        |      / |",
-            "|  \\     |        |     /  |",
-            "|   \\    |        |    /   |",
-            "|    \\   |        |   /    |",
-            "|     \\  |        |  /     |",
-            "|      \\ |        | /      |",
-            "|       \\|        |/       |",
-            "+--------+--------+--------+"
+    String[] glyphMap = {"#________#________#________#",
+            "11_______1________1_______11",
+            "1_1______1________1______1_1",
+            "1__1_____1________1_____1__1",
+            "1___1____1________1____1___1",
+            "1____1___1________1___1____1",
+            "1_____1__1________1__1_____1",
+            "1______1_1________1_1______1",
+            "1_______11________11_______1",
+            "#________#________#________#",
     };
-
-    private static final char POINTER = 'X';
-    private static final char GLYPH = 'G';
-
+    String[] map = {"#________#________#________#",
+            "11_______1________1_______11",
+            "1_1______1________1______1_1",
+            "1__1_____1________1_____1__1",
+            "1___1____1________1____1___1",
+            "1____1___1________1___1____1",
+            "1_____1__1________1__1_____1",
+            "1______1_1________1_1______1",
+            "1_______11________11_______1",
+            "#________#________#________#",
+    };
     private int pointerX = 0;
     private int pointerY = 0;
 
-    public void drawMap(String[] args){
-        for (String row : args) {
-            //System.out.println(row);
-            telemetry.addLine(row);
-        }
-        telemetry.update();
-    }
-
-    public void clearMap(String[] clearThisMap){
-        String[] mapCleared = {"+--------+--------+--------+",
-                "|\\       |        |       /|",
-                "| \\      |        |      / |",
-                "|  \\     |        |     /  |",
-                "|   \\    |        |    /   |",
-                "|    \\   |        |   /    |",
-                "|     \\  |        |  /     |",
-                "|      \\ |        | /      |",
-                "|       \\|        |/       |",
-                "+--------+--------+--------+",
-        };
-
-        for(int copyIteration = 0; copyIteration < clearThisMap.length; ++copyIteration){
-            clearThisMap[copyIteration] = mapCleared[copyIteration];
-        }
-
-        drawMap(clearThisMap);
-    }
-
-    public static void drawPoint(String[] drawTo, String[] drawOn, int pointX, int pointY, char pointChar){
+    public static void drawPoint(String[] drawTo, String[] drawOn, int pointX, int pointY, char pointChar) {
 
         char[] rowToArray = drawOn[pointY].toCharArray();
         rowToArray[pointX] = pointChar;
@@ -99,18 +83,18 @@ public class gLyphitypickerteLeop extends Team2753Linear{
         //System.out.println("Point Drawn");
     }
 
-    public static void clearPoint(String[] map, int pointX, int pointY){
+    public static void clearPoint(String[] map, int pointX, int pointY) {
 
-        String[] mapCleared = {"+--------+--------+--------+",
-                "|\\       |        |       /|",
-                "| \\      |        |      / |",
-                "|  \\     |        |     /  |",
-                "|   \\    |        |    /   |",
-                "|    \\   |        |   /    |",
-                "|     \\  |        |  /     |",
-                "|      \\ |        | /      |",
-                "|       \\|        |/       |",
-                "+--------+--------+--------+",
+        String[] mapCleared = {"#________#________#________#",
+                "11_______1________1_______11",
+                "1_1______1________1______1_1",
+                "1__1_____1________1_____1__1",
+                "1___1____1________1____1___1",
+                "1____1___1________1___1____1",
+                "1_____1__1________1__1_____1",
+                "1______1_1________1_1______1",
+                "1_______11________11_______1",
+                "#________#________#________#",
         };
 
         char[] clearedRowToArray = mapCleared[pointY].toCharArray();
@@ -122,20 +106,20 @@ public class gLyphitypickerteLeop extends Team2753Linear{
         //System.out.println("Point Removed");
     }
 
-    public static void drawPointer(String[] drawTo, String[] drawOn){
+    public static void drawPointer(String[] drawTo, String[] drawOn) {
 
     }
 
-    public static void copyMapTo(String[] copyFrom, String[] copyTo){
+    public static void copyMapTo(String[] copyFrom, String[] copyTo) {
 
-        if(copyFrom.length == copyTo.length) {
+        if (copyFrom.length == copyTo.length) {
             for (int copyIteration = 0; copyIteration < copyFrom.length; ++copyIteration) {
                 copyTo[copyIteration] = copyFrom[copyIteration];
             }
         }
     }
 
-    public static void clearPointer(String[] map, int pointX, int pointY){
+    public static void clearPointer(String[] map, int pointX, int pointY) {
 
         char[] clearedRowToArray = map[pointY].toCharArray();
         char replacingChar = clearedRowToArray[pointX];
@@ -146,57 +130,127 @@ public class gLyphitypickerteLeop extends Team2753Linear{
         //System.out.println("Point Removed");
     }
 
+    public void drawMap(String[] args) {
+        for (String row : args) {
+            //System.out.println(row);
+            telemetry.addLine(row);
+        }
+    }
+
+    public void clearMap(String[] clearThisMap) {
+        String[] mapCleared = {"#________#________#________#",
+                "11_______1________1_______11",
+                "1_1______1________1______1_1",
+                "1__1_____1________1_____1__1",
+                "1___1____1________1____1___1",
+                "1____1___1________1___1____1",
+                "1_____1__1________1__1_____1",
+                "1______1_1________1_1______1",
+                "1_______11________11_______1",
+                "#________#________#________#",
+        };
+
+        for (int copyIteration = 0; copyIteration < clearThisMap.length; ++copyIteration) {
+            clearThisMap[copyIteration] = mapCleared[copyIteration];
+        }
+
+        drawMap(clearThisMap);
+    }
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
 
         //Init
         drawMap(map);
 
-        //navigation and draw/clear points
-        while(!opModeIsActive()){
+        int lastPointerX, lastPointerY;
 
-            int lastPointerX = pointerX;
-            int lastPointerY = pointerY;
+
+        //the boolean names are probably extremely misleading but i'm too retarded to change them appropriately
+
+        boolean upPressed = true;
+        boolean upLastPressed = false;
+
+        boolean downPressed = true;
+        boolean downLastPressed = false;
+
+        boolean leftPressed = true;
+        boolean leftLastPressed = false;
+        ;
+
+        boolean rightPressed = true;
+        boolean rightLastPressed = false;
+
+        //navigation and draw/clear points
+        while (!opModeIsActive()) {
+
+            lastPointerX = pointerX;
+            lastPointerY = pointerY;
 
             //Pointer Controls
-            if(gamepad1.dpad_up || gamepad2.dpad_up){
-                pointerY++;
-            }
-            else if(gamepad1.dpad_down || gamepad2.dpad_down){
-                pointerY--;
-            }
-            else if(gamepad1.dpad_right || gamepad2.dpad_right){
-                pointerX++;
-            }
-            else if(gamepad1.dpad_left || gamepad2.dpad_left){
-                pointerX--;
+
+            if (gamepad1.dpad_up) {
+                if (upLastPressed != upPressed) {
+                    pointerY++;
+                }
+                upLastPressed = upPressed;
+            } else {
+                upLastPressed = !upPressed;
             }
 
+            if (gamepad1.dpad_down) {
+                if (downLastPressed != downPressed) {
+                    pointerY--;
+                }
+                downLastPressed = downPressed;
+            } else {
+                downLastPressed = !downPressed;
+            }
+
+            if (gamepad1.dpad_right) {
+                if (rightLastPressed != rightPressed) {
+                    pointerX++;
+                }
+                rightLastPressed = rightPressed;
+            } else {
+                rightLastPressed = !rightPressed;
+            }
+
+            if (gamepad1.dpad_left) {
+                if (leftLastPressed != leftPressed) {
+                    pointerX--;
+                }
+                leftLastPressed = leftPressed;
+            } else {
+                leftLastPressed = !leftPressed;
+            }
+
+
             //Pointer Boundaries
-            if(pointerY > 0){
+            if (pointerY > 0) {
                 pointerY = 0;
             }
-            if(pointerY < -9){
+            if (pointerY < -9) {
                 pointerY = -9;
             }
-            if(pointerX > 27){
+            if (pointerX > 27) {
                 pointerX = 27;
             }
-            if(pointerX < 0){
+            if (pointerX < 0) {
                 pointerX = 0;
             }
 
             //Glyph controls
 
-            if(gamepad1.x){
+            if (gamepad1.x) {
                 drawPoint(glyphMap, glyphMap, pointerX, -pointerY, GLYPH);
             }
-            if(gamepad1.b){
+            if (gamepad1.b) {
                 clearPoint(glyphMap, pointerX, -pointerY);
             }
 
             //Clear last Pointer if we moved
-            if((lastPointerX != pointerX) || (lastPointerY != pointerY)){
+            if ((lastPointerX != pointerX) || (lastPointerY != pointerY)) {
                 //clear pointer
                 clearPoint(map, lastPointerX, -lastPointerY);
             }
@@ -209,14 +263,13 @@ public class gLyphitypickerteLeop extends Team2753Linear{
 
             drawMap(map);
             telemetry.update();
-
         }
 
         telemetry.clearAll();
         //output points
 
 
-        sleep(5000);
+        //waitForTick(5000);
         requestOpModeStop();
     }
 }
